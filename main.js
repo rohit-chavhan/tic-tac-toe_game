b2bf9cf77531a223c4c7731eee54a790a5c6f91f
rohit chavhan <rohitchavhan46@gmail.com>
2024-06-17 00:52:20 +0530

    game works on console

--- main.js
+++ main.js
@@ -12,25 +12,53 @@ function Gameboard() {
 
     const getBoard = () => board;
 
+    
     const builtBoard = () => {
         const boardCelss = board.map(row => row.map(eachCell => eachCell.getValue()))
         console.log(boardCelss);
+        return boardCelss;
     }
 
     const makeMove = (mainArray, insieElement, move) => {
         board[mainArray][insieElement].addMove(move);
     }
 
+    const checkWinner = () => {
+        let mainArr = [];
+        let threeRow = board.map(row => row.map(eachCell => eachCell.getValue()));
+
+        let columnArr = threeRow[0].map((_, colIndex) => threeRow.map(row => row[colIndex]));
+
+        let diagonalOne = threeRow[0].map((_, index) => threeRow[index][index]);
+        mainArr.push(diagonalOne);
+
+        let ray = [threeRow, columnArr];
+
+        const mainArray = ray.forEach(el => el.forEach(eachEl => mainArr.push(eachEl)));
+
+        ;(function(){
+            let mainRay = [];
+            for(let j = 0;j < threeRow.length; j++ ){
+                let oneIndex = threeRow.length - 1;
+                mainRay.push(threeRow[oneIndex][j]);
+            };
+            mainArr.push(mainRay);
+        })();
+
+        return mainArr;
+    }
+
     return {
         getBoard,
         builtBoard,
-        makeMove
+        makeMove,
+        checkWinner
     }
 }
 
 
 function Cell() {
-    let value = "*";
+    let value = "_";
 
     const addMove = (player) => {
         value = player;
@@ -76,25 +104,39 @@ function gamePlay(
     }
 
     let count = 0;
-    const countMoves = () => {
-        return () => ++count;
-    }
 
     const playRound = (x,y) => {
+        if(count === 'over') {
+            return;
+        }
+
         console.log(
             `${getActivePlayer().name} making a move with token ${getActivePlayer().token} in main Arr ${x} ${y} ...`
         );
 
         board.makeMove(x,y, getActivePlayer().token);
-        
+
+        count += 1; 
+        if(count > 4) {
+            ;(function (){
+               let allMatchPatterns =  board.checkWinner();
+               console.log(allMatchPatterns, getActivePlayer().token)
+
+               let a = allMatchPatterns.filter(el => el.every(eachEl => eachEl === getActivePlayer().token));
+               console.log(a);
+               count = "over";
+
+                if(a.length) {
+                    console.log(`${getActivePlayer().name} is a winner, game over`);
+                    return;
+                }
+            })();
+        }
+
+        if(count === 'over') {
+            return
+        }
         switchPlayerTurn();
-
-        /*
-        here to add iffe function which will create evry possible row
-        with then it will check wheter each element in row is same as user typed token and if that it will print winer or looser
-        */
-
-        /* prin new round will print the arrays in number which is * x y */
         printNewRound();
     }
 
@@ -107,14 +149,10 @@ function gamePlay(
 }
 
 const red = gamePlay();
+red.playRound(0,0);
+red.playRound(1,0);
 red.playRound(0,1);
-red.playRound(0,1);
-red.playRound(0,1);
-red.playRound(0,1);
-red.playRound(0,1);
-red.playRound(0,1);
-red.playRound(0,1);
-// let red = Cell();
-// console.log(red);
-// red.addToken("red");const 
-// console.log(red.getValue());
+red.playRound(1,1);
+red.playRound(0,2);
+red.playRound(0,2);
+
